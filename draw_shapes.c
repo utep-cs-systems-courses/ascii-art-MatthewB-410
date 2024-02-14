31240808b838cfb5ac0d9592a73a4f482c75ea1b
Matthew Barahona <mjbarahona@miners.utep.edu>
2024-02-13 17:38:10 -0700

    finished product

--- draw_shapes.c
+++ draw_shapes.c
@@ -8,7 +8,7 @@ void print_square(int leftCol, int size)
   int endCol = leftCol + size;
   for (int row = 0; row < size; row++){
     int col;
-    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (col = 0; col < leftCol; col++) putchar(' '); printf("    ");
     for (       ; col < endCol;  col++) putchar('*');
     putchar('\n');
   }
@@ -26,3 +26,8 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+void print_arrow(int leftCol, int size){
+  print_triangle(leftCol, size);
+  print_square(leftCol, size);
+  putchar('\n');
+    }
